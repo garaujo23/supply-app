97302e71c4db298534b7153b95a6f2f72ade4618
Y07yoyo <40599534+Y07yoyo@users.noreply.github.com>
2018-07-02 22:27:03 +1000

    Add files via upload
    Added an Appbar to the app

---
+++ flutter_app/lib/ui/home.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("DDILABS"),
+        centerTitle: true,
+        backgroundColor: Colors.black,
+      ),
+      body: new Container(
+        color: Colors.blue.shade200,
+        
+      ),
+
+    );
+  }
+}
+
