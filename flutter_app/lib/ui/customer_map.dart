a328b287b7e33df14e82cd1d936d8f60340acc06
Y07yoyo <40599534+Y07yoyo@users.noreply.github.com>
2018-09-02 10:13:33 +1000

    Fixed initial error message on maps and added static map updates on change of address

--- flutter_app/lib/ui/customer_map.dart
+++ flutter_app/lib/ui/customer_map.dart
@@ -1,20 +1,18 @@
-import 'dart:async';
-
-import 'package:flutter/material.dart';
-import 'package:map_view/figure_joint_type.dart';
-import 'package:map_view/map_view.dart';
-import 'package:map_view/polygon.dart';
-import 'package:map_view/polyline.dart';
-import '../ui/login_screen.dart';
-import 'package:firebase_database/firebase_database.dart';
-import 'package:http/http.dart' as http;
 import 'dart:convert';
+
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:location/location.dart' as geoloc;
+import 'package:map_view/map_view.dart';
 
 import '../main.dart';
+import '../ui/ensure_visible.dart';
+import '../ui/login_screen.dart';
 
 class CustomerMap extends StatefulWidget {
   final FirebaseDatabase database = FirebaseDatabase.instance;
+
   @override
   _CustomerMapState createState() => _CustomerMapState();
 }
@@ -25,22 +23,33 @@ class _CustomerMapState extends State<CustomerMap> {
   //var staticMapProvider = new StaticMapProvider(API_KEY);
   Uri _staticMapUri;
   LocationData _locationData;
+  final FocusNode _addressInputFocusNode = FocusNode();
+  final TextEditingController _addressInputController = TextEditingController();
 
   @override
   initState() {
-    getStaticMap();
+    //getStaticMap();
+    getStaticMap(data['Company Address']);
+    _addressInputFocusNode.addListener(_updateLocation);
     super.initState();
 //    cameraPosition = new CameraPosition(Locations.portland, 2.0);
 //    staticMapUri = staticMapProvider.getStaticUri(Locations.portland, 12,
 //        width: 900, height: 400, mapType: StaticMapViewType.roadmap);
   }
 
-  void getStaticMap() async {
+  void getStaticMap(String address) async {
+    //String address = '${data['Company Address']}';
+    if (address.isEmpty) {
+      return;
+    }
     // String API_KEY = ''; //This is already defined in main.dart so therefore do not need this here again
     final location = geoloc.Location();
     final currentLocation = await location.getLocation();
     //print(data['Company Address']); This prints null
-    final Uri uri = Uri.https('maps.googleapis.com', '/maps/api/geocode/json', {'address': '${data['Company Address']}', 'key': API_KEY}); //Address hardcoded for now until data issue sorted
+    final Uri uri = Uri.https('maps.googleapis.com', '/maps/api/geocode/json', {
+      'address': address,
+      'key': API_KEY
+    }); //Address hardcoded for now until data issue sorted
     final http.Response response = await http.get(uri);
     final decodedResponse = json.decode(response.body);
     //print(decodedResponse);
@@ -49,25 +58,34 @@ class _CustomerMapState extends State<CustomerMap> {
     //print(decodedResponse);
     final coords = decodedResponse['results'][0]['geometry']['location'];
     _locationData = LocationData(
-      //You can toggle between user location and set location using geocode
+        //You can toggle between user location and set location using geocode
         address: formattedAddress,
         latitude: coords['lat'],
         //latitude: currentLocation['latitude'],
         longitude: coords['lng']);
-        //longitude: currentLocation['longitude']);
-
+    //longitude: currentLocation['longitude']);
 
     final StaticMapProvider staticMapProvider = StaticMapProvider(API_KEY);
     final Uri staticMapUri = staticMapProvider.getStaticUriWithMarkers([
-      Marker('position', 'Position', _locationData.latitude, _locationData.longitude),
-    ], center: Location(_locationData.latitude, _locationData.longitude),
-    width: 900, height: 400,
-    maptype: StaticMapViewType.roadmap);
+      Marker('position', 'Position', _locationData.latitude,
+          _locationData.longitude),
+    ],
+        center: Location(_locationData.latitude, _locationData.longitude),
+        width: 900,
+        height: 300,
+        maptype: StaticMapViewType.roadmap);
     setState(() {
+      _addressInputController.text = _locationData.address;
       _staticMapUri = staticMapUri;
     });
   }
 
+  void _updateLocation() {
+    if (!_addressInputFocusNode.hasFocus) {
+      getStaticMap(_addressInputController.text);
+      //getStaticMap(data['Company Address']);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -80,10 +98,27 @@ class _CustomerMapState extends State<CustomerMap> {
         body: new Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
+            EnsureVisibleWhenFocused(
+              focusNode: _addressInputFocusNode,
+              child: TextFormField(
+                focusNode: _addressInputFocusNode,
+                controller: _addressInputController,
+                validator: (String value) {
+                  if (_locationData == null || value.isEmpty) {
+                    return 'No valid location found.';
+                  }
+                },
+                decoration: InputDecoration(labelText: 'Address'),
+              ),
+            ),
+
             SizedBox(
               height: 10.0,
             ),
-            Image.network(_staticMapUri.toString()) //this throws an error first time around, need to look at
+            _staticMapUri == null
+                ? Container()
+                : Image.network(_staticMapUri
+                    .toString()) //this throws an error first time around, need to look at
           ],
 //          children: <Widget>[
 //            new Container(
